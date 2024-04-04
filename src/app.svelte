3f6495b1e87dd9a6d61c68263c6a0e64cec1d970
Tom Wilson <tom@hyper.io>
2024-04-04 01:42:25 +0000

    update grid view

--- src/app.svelte
+++ src/app.svelte
@@ -5,7 +5,14 @@
   const pid = "03I7E-3wkTZa__Bn1Qq5flYrtEQ7NkcoD9Ctg4o2mNI";
   let items = [];
   onMount(() => {
-    setInterval(getGameInfo, 2000);
+    let data = [];
+    for (var row = 1; row <= 40; row++) {
+      for (var col = 1; col <= 40; col++) {
+        data.push({ x: row, y: col, active: false });
+      }
+    }
+    items = data;
+    setTimeout(getGameInfo, 2000);
   });
 
   window.getGameInfo = async () => {
@@ -16,24 +23,53 @@
       data: "",
     });
     let gameState = JSON.parse(result.Messages[0].Data);
-    items = Object.values(gameState.Players);
+    console.log(gameState);
+    items = items.map((c) => {
+      c.active = false;
+      c.health = 0;
+      return c;
+    });
+    Object.values(gameState.Players).forEach((p) => {
+      items = items.map((c) => {
+        if (c.x === p.x && c.y === p.y) {
+          c.active = true;
+          c.health = p.health;
+        }
+        return c;
+      });
+    });
+    setTimeout(getGameInfo, 2000);
   };
+
+  function showCell(active, health) {
+    if (active) {
+      if (health > 50) {
+        return "bg-green-500";
+      } else {
+        return "bg-red-200";
+      }
+    } else {
+      return "";
+    }
+  }
 </script>
 
 <h1 class="text-3xl text-center">Grid View</h1>
-<div class="grid grid-cols-40 gap-1">
-  {#each items as { x, y }}
-    <div
-      class="grid-item bg-green-500"
-      style="grid-row-start: {y}; grid-column-start: {x};"
-    ></div>
-  {/each}
+<div class="flex items-center justify-center h-screen">
+  <div class="grid grid-cols-40 w-2/3">
+    {#each items as { x, y, active, health }}
+      <div
+        class="grid-item border-1 {showCell(active, health)}"
+        style="grid-row-start: {y}; grid-column-start: {x};"
+      ></div>
+    {/each}
+  </div>
 </div>
 
 <style>
   .grid-item {
     /* Adjust the width and height as needed */
-    width: 20px;
-    height: 20px;
+    width: 30px;
+    height: 30px;
   }
 </style>
